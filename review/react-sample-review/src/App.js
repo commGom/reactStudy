cb55073071ef577f19e24e4464f400f16bda7526
RoarinGom <hyunwoo921129@gmail.com>
2021-12-09 18:26:10 +0900

    컴포넌트 분리 및 화면 출력만 구현

--- review/react-sample-review/src/App.js
+++ review/react-sample-review/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Article from './components/Article';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+      <Section></Section>
+      <Article></Article>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ review/react-sample-review/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>야구 게임 방식</h1>
+                <hr></hr>
+            </div>
+        );
+    }
+}
+
+export default Header;
