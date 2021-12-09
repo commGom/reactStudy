781f1d6718c418f83f0d18c1ca97852cf7d90ae4
RoarinGom <hyunwoo921129@gmail.com>
2021-12-09 17:50:14 +0900

    class, function component props, state, useState 이용

--- react-app-component/src/App.js
+++ react-app-component/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from 'react';
+
+class ClassComp extends Component {
+  constructor(props) {
+    super(props);
+    // props 데이터를 state 데이터에 담음, 확장성을 위하여 
+    this.state = { initNumber: this.props.initNumber }
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>Class Style Component</h2>
+        <p>Numbers from class component:{this.state.initNumber}</p>
+      </div>
+    )
+  }
+}
+
+function FunctionComp(props) {
+  const num = useState(props.initNumber);
+  console.log(num);
+  return (
+    <div className="container">
+      <h2>Function Style Component</h2>
+      <p>Numbers from function component:{num[0]}</p>
+
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World</h1>
+      <ClassComp initNumber={2}></ClassComp>
+      <FunctionComp initNumber={2}></FunctionComp>
     </div>
   );
 }
